3841913b1b531d12e3325d0d7d33ceef0283a1db
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-06-10 14:55:37 -0500

    Adding The Timer & Difficulty Setting

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -4,7 +4,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -38,6 +38,15 @@ let randomWord;
 let score = 0;
 /* Init Time */
 let time = 10;
+/* Set difficulty to value in ls or medium */
+let difficulty = localStorage.getItem('difficulty') !== null 
+                    ? localStorage.getItem('difficulty')
+                        : 'medium';
+// Set difficulty select value
+difficultySelect.value = difficulty;
+/* Focus on text on start */
+text.focus();
+
 /* GET RANDOM WORD */
 const getRandomWord = () => {
     return words[Math.floor(Math.random() * words.length)];
@@ -50,11 +59,34 @@ const addWordToDOM = () => {
 /* UPDATE SCORE */
 const updateScore = () => {
     score++;
-    scoreEl.innerHTML = score;
+    scoreEl.innerText = score;
 };
+/* UPDATE TIME */
+const updateTime = () => {
+    time--;
+    timeEl.innerText = time + 's';
+    if(time === 0) {
+        clearInterval(timeInterval);
+        // End Game
+        gameOver();
+    }
+};
+/* GAME OVER, SHOW END SCREEN */
+const gameOver = () => {
+    endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+    endgameEl.style.display = 'flex';
+};
+/* Start counting down */
+const timeInterval = setInterval(updateTime, 1000);
+
 /* ************************************************************ */
 const eventos = () => {
     console.log('Event Listeners');
+    // Typing
     text.addEventListener('input', (e) => {
         const insertedText = e.target.value;
         if (insertedText === randomWord) {
@@ -62,8 +94,30 @@ const eventos = () => {
             updateScore();
             // Clear
             e.target.value = '';
+
+            if (difficulty === 'hard') {
+                time += 2;
+            } else if (difficulty === 'medium') {
+                time += 3;
+            } else {
+                time += 4;
+            }
+            
+            updateTime();
         }
     });
+
+    // Settings btn click
+    settingsBtn.addEventListener('click', () => {
+        settings.classList.toggle('hide');
+    });
+
+    // Setting select
+    settingsForm.addEventListener('change', (e) => {
+        difficulty = e.target.value;
+       localStorage.setItem('difficulty', difficulty);
+    });
+
 };
 /* ************************************************************ */
 const init = () => {
